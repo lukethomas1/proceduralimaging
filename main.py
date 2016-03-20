d54721db4e1fb8492708ca01da82f81804a7e521
Luke Thomas <lukethethomas@gmail.com>
2016-03-20 15:12:17 -0700

    Revert "Changed an error message ancd commented a bit"
    This reverts commit 50f26d06a194b86c72c4cb582a5d1e2ece9705ed.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ width = 0;
 height = 0;
 
 # Volatility, the amount the colors change each cycle
-volatility = 0;
+changeAmount = 0;
 
 # Used for user specifications
 rgbOption = ""
@@ -51,29 +51,22 @@ while input("Type any key to create a picture or q to quit: ") != "q":
     # Get valid height and width input from user
     while(True):
         try:
-        	# Get width and height
             width = int(input("Enter a width: "))
             height = int(input("Enter a height: "))
-
-            # Get volatility
-            volatility = int(input("Enter volatility: "))
-
+            changeAmount = int(input("Enter volatility: "))
             # Ask if user wants to specify starting RGB values or randomize
             rgbOption = input("Starting RGB values, (r)andom or (c)ustom: ")
             settingsOption = input("Settings: (d)efault or (c)ustom? ")
             imageAmount = int(input("How many images would you like to" +
                                     " create? "))
 
-            # Make sure input is valid
             if((rgbOption != "r" and rgbOption != "c") or
                (settingsOption != "d" and settingsOption != "c")):
                 raise ValueError()
             valid = True
-
-            # Break out of the while loop if no errors
             break;
         except Exception:
-            print("Invalid input, try again.")
+            print("Invalid width or height")
 
     rgb = create.InitialColors(rgbOption)
     settings = create.ImageSettings(settingsOption)
@@ -89,8 +82,7 @@ while input("Type any key to create a picture or q to quit: ") != "q":
     batch = './images/batch' + str(index)
     makedirs(batch)
 
-    # Create the images and save them to the batch folder
     for number in range(1, imageAmount + 1):
-        img = create.NewImage(width, height, rgb, <div>volatility</div>,
+        img = create.NewImage(width, height, rgb, changeAmount,
                               separateVariance, overlap)
         img.save(batch + '/' + str(number) + '.bmp')
